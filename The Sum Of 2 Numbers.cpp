60d88e8906833bbee2be03bf2ea0ab92cf6784dd
Sujal_Kandhai <81705740+thsd-usi@users.noreply.github.com>
2024-11-25 16:27:40 -0300

    Add files via upload

---
+++ The Sum Of 2 Numbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+                                    double x, y , sum;
+   
+      cout << "enter your first number";
+      cin >> x;
+
+      cout << "enter your second number ";
+      cin >> y;
+       
+
+       sum= x + y;
+
+  
+       cout <<"the sum of the two numbers is "<< x <<" and "<< y <<" is: "<<sum<<endl;
+    
+    
+
+    return 0;
+}
