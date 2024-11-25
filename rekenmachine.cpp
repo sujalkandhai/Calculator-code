65d3f871ab40e58a56f41384d50648d101f2d96b
Sujal_Kandhai <81705740+thsd-usi@users.noreply.github.com>
2024-11-25 16:24:07 -0300

    Add files via upload

---
+++ rekenmachine.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char operation;      
+    double num1, num2;   
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    cout << "Choose an operation (+, -, *, /): ";
+    cin >> operation;
+
+    switch (operation) {
+        case '+':
+            cout << "Result: " << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << "Result: " << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << "Result: " << num1 * num2 << endl;
+            break;
+        case '/':
+            if (num2 != 0) {  
+                cout << "Result: " << num1 / num2 << endl;
+            } else {
+                cout << "Error: Division by zero is not allowed!" << endl;
+            }
+            break;
+        default:
+            cout << "Error: Invalid operator! Please use +, -, *, or /." << endl;
+            break;
+    }
+
+    return 0;
+}
